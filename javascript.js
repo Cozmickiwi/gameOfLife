e35fe55c9910843de3813a649b81d59b01b27cb9
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-16 21:43:22 +1300

    made game work properly

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 const gameContainer = document.querySelector('.gameContainer');
 const startButton = document.querySelector('.startButton');
+let liveCells = [];
+let deadCells = [];
 function gridGen(){
     for(i=1;i<=2500;i++){
         let pixel = document.createElement('div');
         pixel.setAttribute('class', 'pixel');
         pixel.setAttribute('id', i);
         pixel.style.backgroundColor = 'white';
-        pixel.addEventListener('click', () =>{
-            pixel.style.backgroundColor = 'black';
+        pixel.addEventListener('mousemove', event =>{
+            if (event.buttons == 1){
+                pixel.style.backgroundColor = 'black';
+            }
             //console.log(pixel.id);
         })
         
@@ -25,13 +29,13 @@ function game(curPixel){
     let nENeighbor;
     let sWNeighbor;
     let sENeighbor
-    if((Number(curPixel.id) - 1) % 10 != 0){
+    if((Number(curPixel.id) - 1) % 50 != 0){
         leftNeighbor = document.getElementById(`${Number(curPixel.id) - 1}`);
         if(leftNeighbor.style.backgroundColor == 'black'){
             neighborList.push('left');
         }
     }
-    if(Number(curPixel.id) % 10 != 0){
+    if(Number(curPixel.id) % 50 != 0){
         rightNeighbor = document.getElementById(`${Number(curPixel.id) + 1}`);
         if(rightNeighbor.style.backgroundColor == 'black'){
             neighborList.push('right');
@@ -49,36 +53,36 @@ function game(curPixel){
             neighborList.push('bottom');
         }
     }
-    if(Number(curPixel.id) > 50 && (Number(curPixel.id) - 1) % 10 != 0){
+    if(Number(curPixel.id) > 50 && (Number(curPixel.id) - 1) % 50 != 0){
         nWNeighbor = document.getElementById(`${Number(topNeighbor.id) - 1}`);
         if(nWNeighbor.style.backgroundColor == 'black'){
             neighborList.push('north west');
         }
     }
-    if(Number(curPixel.id) > 50 && Number(curPixel.id) % 10 != 0){
+    if(Number(curPixel.id) > 50 && Number(curPixel.id) % 50 != 0){
         nENeighbor = document.getElementById(`${Number(topNeighbor.id) + 1}`);
         if(nENeighbor.style.backgroundColor == 'black'){
             neighborList.push('north east');
         }
     }
-    if(Number(curPixel.id) <= 2450 && (Number(curPixel.id) - 1) % 10 != 0){
+    if(Number(curPixel.id) <= 2450 && (Number(curPixel.id) - 1) % 50 != 0){
         sWNeighbor = document.getElementById(`${Number(bottomNeighbor.id) - 1}`);
         if(sWNeighbor.style.backgroundColor == 'black'){
             neighborList.push('south west');
         }
     }
-    if(Number(curPixel.id) <= 2450 && Number(curPixel.id) % 10 != 0){
+    if(Number(curPixel.id) <= 2450 && Number(curPixel.id) % 50 != 0){
         sENeighbor = document.getElementById(`${Number(bottomNeighbor.id) + 1}`);
         if(sENeighbor.style.backgroundColor == 'black'){
             neighborList.push('south east');
         }
     }
-    if(neighborList.length < 2 || neighborList.length > 3 && curPixel.style.backgroundColor == 'black'){
-        curPixel.style.backgroundColor = 'white';
+    if((neighborList.length < 2 && curPixel.style.backgroundColor == 'black') || (neighborList.length > 3 && curPixel.style.backgroundColor == 'black')){
+        deadCells.push(curPixel);
         return('done');
     }
     else if(curPixel.style.backgroundColor == 'white' && neighborList.length == 3){
-        curPixel.style.backgroundColor = 'black';
+        liveCells.push(curPixel);
         return('done');
     }
     else{
@@ -90,14 +94,29 @@ function gameLoop(){
     for(i=1;i<=2500;i++){
         game((document.getElementById(i)));
     }
+    cellChanger();
+    return('a');
+}
+
+function cellChanger(){
+    if(liveCells.length > 0){
+        liveCells.forEach((cell, index) => {
+            liveCells[index].style.backgroundColor = 'black';
+            
+        })
+    }
+    if(deadCells.length > 0){
+        deadCells.forEach((cell, index) => {
+            deadCells[index].style.backgroundColor = 'white';
+        })
+    }
+    liveCells = [];
+    deadCells = [];
+    return('done');
 }
 
 startButton.addEventListener('click', () => {
-    for(a=1;a<100;a++){
-        setTimeout(function(){
-            gameLoop();
-        }, 1000)
-    }
+    setInterval(() => {gameLoop()}, 200);
 })
 
 gridGen();
